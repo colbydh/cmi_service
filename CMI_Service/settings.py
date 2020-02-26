6deaec0427b367d61e57539e18f91dcefeedbc16
Josh Lyons <lyons_joshua@bah.com>
2020-02-26 15:30:21 -0600

    Trying to get azure to work....

--- CMI_Service/settings.py
+++ CMI_Service/settings.py
@@ -94,11 +94,11 @@ DATABASES = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DATABASE_NAME", ''),
-        "USER": os.environ.get("DATABASE_USER", ""),
-        "PASSWORD": os.environ.get("DATABASE_PASSWORD", ""),
-        "HOST": os.environ.get("DATABASE_HOST", ""),
-        "PORT": os.environ.get("DATABASE_PORT", ""),
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": os.environ["DATABASE_HOST"],
+        "PORT": os.environ["DATABASE_PORT"],
         "OPTIONS": {
             "sslmode": "require"
         }
